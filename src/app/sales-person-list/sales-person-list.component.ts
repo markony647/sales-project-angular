540a5ef4ed8cba7142fb3a0e97af06efe0d010de
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-06-10 17:38:20 +0300

    Add custom component and html

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("John", "Doe", "dsf@gmail.com", 1000),
+    new SalesPerson("John2", "Ja", "ja@gmail.com", 3000),
+    new SalesPerson("John3", "Ra", "drafd@gmail.com", 5000),
+    new SalesPerson("John", "Sam", "dasdsad@gmail.com", 200),
+    new SalesPerson("John", "Doe", "dsf@gmail.com", 4000)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
