c0c675ea4e80cba6e4b1869a13ae6fe6e33c054b
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-06-14 17:32:19 +0300

    Add boostrap styles

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -3,7 +3,7 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
@@ -13,7 +13,8 @@ export class SalesPersonListComponent implements OnInit {
     new SalesPerson("John2", "Ja", "ja@gmail.com", 3000),
     new SalesPerson("John3", "Ra", "drafd@gmail.com", 5000),
     new SalesPerson("John", "Sam", "dasdsad@gmail.com", 200),
-    new SalesPerson("John", "Doe", "dsf@gmail.com", 4000)
+    new SalesPerson("John", "Doe", "dsf@gmail.com", 4000),
+    new SalesPerson("John2", "Doe2", "dsf@gmail.com", 42000)
   ];
 
   constructor() { }
